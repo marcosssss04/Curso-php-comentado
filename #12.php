ceacf044f39c2c7525f9db16c9cfb3acdd534025
marcos aurelio <marcosssss1991@gmail.com>
2020-04-09 21:56:38 -0300

    curso

---
+++ #12.php
@@ -0,0 +1,33 @@
+<?php
+///////////// array_unshift adiciona um ou mais elemento no inicio do array
+
+/*
+$frutas = array("uva", "laranja", "maça");
+print_r($frutas);
+array_unshift($frutas, "manga", "Acerola", "Morango");// funçao para adiciona no array
+echo "<br>";
+print_r($frutas);
+*/
+
+/*
+$frutas = array("manga", "morango", "laranja");
+print_r($frutas);
+array_push($frutas, "acerola", "mamao", "jaca");// adiciona um ou mais elementos no final do array
+echo "<br>";
+print_r($frutas);
+*/
+
+/*
+$keys = array("Campeao", "vice", "terceiro");
+$values = array("flamengo", "vasco", "botafogo");
+
+$times = array_combine($keys, $values);// mescla os dois array 
+print_r($times);
+*/
+
+/*
+$soma = array(5,20,40,83);
+$total =  array_sum($soma); // soma os valores de um array
+echo $total;
+*/
+
