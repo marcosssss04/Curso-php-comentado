62bf6321d1227ddd7a1d9da738d132237259a1ff
marcos aurelio <marcosssss1991@gmail.com>
2020-04-09 22:53:03 -0300

    curso

---
+++ #15.php
@@ -0,0 +1,26 @@
+<?php
+
+//////switch case parecido com o if 
+$cor = "amarelo";
+ 
+switch ($cor):
+    case "vermelho":
+    echo "Sua cor preferida é o vermelho";
+    break;
+
+    case "verde":
+    echo "Sua cor preferida é a verde";
+    break;
+
+    case "azul":
+    echo "Sua cor preferida é a azul";
+    break;
+
+    default:
+    echo "Sua cor preferida nao é vermelho, verde, ou azul";
+
+endswitch;
+
+
+
+   
